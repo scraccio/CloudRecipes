7b465866a861dfcd547b2279467cdebbcf019d65
Simone Costanzo <95088305+scraccio@users.noreply.github.com>
2025-12-15 17:26:39 +0100

    Create processor.py

---
+++ image-processor/processor.py
@@ -0,0 +1,36 @@
+import os
+import boto3
+from PIL import Image
+import io
+
+s3 = boto3.client("s3")
+
+def main():
+    bucket = os.environ["BUCKET"]
+    key = os.environ["KEY"]
+
+    print(f"processando s3://{bucket}/{key}")
+
+    obj = s3.get_object(Bucket=bucket, Key=key)
+    image_bytes = obj["Body"].read()
+
+    image = Image.open(io.BytesIO(image_bytes))
+    image.thumbnail((512, 512))
+
+    buffer = io.BytesIO()
+    image.save(buffer, format=image.format)
+    buffer.seek(0)
+
+    resized_key = f"resized/{key}"
+
+    s3.put_object(
+        Bucket=bucket,
+        Key=resized_key,
+        Body=buffer,
+        ContentType=obj["ContentType"]
+    )
+
+    print(f"salvando l'immagine resized su s3://{bucket}/{resized_key}")
+
+if __name__ == "__main__":
+    main()
